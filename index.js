e51a8595dad7268dceb39408f7c70606ea9c8fa5
rejo132 <enoch.luke1@student.moringaschool.com>
2025-03-20 22:50:37 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// index.js
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
